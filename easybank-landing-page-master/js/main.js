59c89946908156ca8547956755b41f81306ae142
Aya Habeeb <aya.r.h98@gmail.com>
2020-05-15 00:04:02 +0300

    Finished the Second challenge

---
+++ easybank-landing-page-master/js/main.js
@@ -0,0 +1,27 @@
+const toggleBtn = document.getElementById('toggle');
+const menu = document.getElementById('navbar');
+const image = document.getElementById('icon-hamburger');
+const closeIcon = "./images/icon-close.svg";
+const openIcon = "./images/icon-hamburger.svg";
+let toggle = true;
+
+toggleBtn.addEventListener('click', function(){
+    menu.classList.toggle('open');
+
+    if (toggle === true) {
+        image.src  = closeIcon;
+    } else {
+        image.src= openIcon;
+    }
+
+    toggle = !toggle;
+});
+
+function checker() {
+    if(screen.width > 768) {
+        menu.classList.remove('open');
+        image.src= openIcon;
+    }
+}
+
+window.addEventListener('resize', checker);
